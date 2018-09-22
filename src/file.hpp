5b8959b3658e33ee070c628281ca6413e5c1f1fb
samuel <guerriernumerique@gmailcom>
2018-09-23 01:18:03 +0200

    Ajout de l'application du pendu

---
+++ src/file.hpp
@@ -0,0 +1,14 @@
+#ifndef FILE_HPP 
+#define FILE_HPP
+
+ 
+extern std::size_t getFileLineTotal(std::ifstream& fileStream); 
+extern std::string getFileLine(std::ifstream& fileStream, const std::size_t line);
+
+extern std::string generateWord(std::string fileName);
+extern std::string reverseWord(const std::string& word);
+
+ 
+ 
+
+#endif // FILE_HPP
